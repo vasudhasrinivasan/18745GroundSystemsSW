8ec566033351c453f828c226ab6eb0fc80a0f452
Rene Carballo <rcarballo2222@gmail.com>
2023-04-17 16:44:28 -0400

    Add files via upload

--- uwb/uwb_calc.py
+++ uwb/uwb_calc.py
@@ -1,28 +1,21 @@
 import numpy as np
 import math
 from scipy.optimize import least_squares
-import time
-import random
 
-### CHANGE
+"""
+CONSTANTS
+"""
 ANCHOR_POINTS = [(0,0), (10,0), (10,10), (0,10)]    #Coordinates of anchor points (m)
-###
+ROUND_DIGITS = 5                                    #Number of digits to round to
 
-###
+"""
+GLOBAL VARS
+"""
+prev_tag = [-1,-1]    # Keep track of previous tag position to calculate angle
 
-prev_tag = [0,0]
-
-def calculate_distance(x1, y1, x2, y2):
-    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
-
-def get_distances(tag, anchor_points):
-
-  distances = []
-  distances.append(calculate_distance(tag[0], tag[1], anchor_points[0][0], anchor_points[0][1]))
-  distances.append(calculate_distance(tag[0], tag[1], anchor_points[1][0], anchor_points[1][1]))
-  distances.append(calculate_distance(tag[0], tag[1], anchor_points[2][0], anchor_points[2][1]))
-  distances.append(calculate_distance(tag[0], tag[1], anchor_points[3][0], anchor_points[3][1]))
-  return distances
+"""
+HELPER FUNCTIONS
+"""
 
 def residual(x, circles):
     err = []
@@ -36,29 +29,31 @@ def get_tag_coords(anchor_points, distances):
 
     # Find the intersection points using least squares method
     res = least_squares(residual, [0, 0], args=(circles,))
-    points = [(round(res.x[0], 5), round(res.x[1], 5))]
+    points = [(round(res.x[0], ROUND_DIGITS), round(res.x[1], ROUND_DIGITS))]
 
     return points[0]
     
 def get_tag_direction(tag):
     return math.degrees(math.atan((prev_tag[1] - tag[1])/(prev_tag[0] - tag[0])))
 
+"""
+
+FUNCTIONS
+
+"""
+
 def get_tag_info(distances):
+    """
+    Calculates tag position and degrees from previous position based on anchor point distances
+    `distances`(list): Distance from each anchor point to the tag (m)
+    
+    Returns: ((x, y), deg)
+    """
     tag = get_tag_coords(ANCHOR_POINTS, distances)
     deg = get_tag_direction(tag)
     prev_tag = tag  #update prev to new tag data
     return [tag, deg]
 
-
-#Test
-"""
-test_tag = [1.0, 1.0]
-while(1):
-    test_tag[0] += random.random()
-    test_tag[1] += random.random()
-    distances = get_distances(test_tag, ANCHOR_POINTS)
-    tag_info = get_tag_info(distances)
-    print(f"Actual:[{test_tag}, Calculated:{tag_info}]")
-
-    time.sleep(1)
-"""
+# Sample Usage
+# distances = [0, 10, 10*math.sqrt(2), 10] # For a tag at position (0,0)
+# print(get_tag_info(distances))
